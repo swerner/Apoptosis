fd2b92cd8c3f40ec0e9127c9fc6231c92ab9f64a
Scott Werner <stwerner@vt.edu>
2011-08-04 07:34:05 -0400

    Removing package directory

--- pkg/apoptosis-0.0.2/lib/apoptosis.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'date'
-require 'apoptosis/apoptosis.rb'
-
-module Apoptosis
-  VERSION = '0.0.2'
-end
